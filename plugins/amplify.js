7490ba5d7aa99da363f1df9d69b195c913c53d99
ielb <i.elbouhatii@gmail.com>
2022-03-11 01:51:13 +0100

    need help

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,4 +1,3 @@
 import {Amplify} from 'aws-amplify'
-import '@aws-amplify/ui-vue';
-import config from '../src/aws-exports'
-Amplify.configure(config);
+import awsconfig from '~/aws-exports'
+Amplify.configure(awsconfig);
